d82df479104ea344a2a82cd21d67bae16fd75ebd
DaMangoHaha <stephenmangum01@gmail.com>
2025-04-04 09:00:07 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
 ﻿namespace Classes_Mangum_Stephen
 {
-    internal class Program
+    using System;
+
+// Greetings class with two public methods
+public class Greetings
+{
+    // Method to print a welcome message
+    public void Welcome()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("You know...we had a Go Green assembly today...");
     }
+
+    // Method to print a personalized message
+    public void Hello(string name)
+    {
+        Console.WriteLine($"I don't think I saw YOU there, {name}...");
+    }
+}
+
+// Program class with Main method
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creates an instance of Greetings
+        Greetings greetings = new Greetings();
+
+        // Calls the Welcome method
+        greetings.Welcome();
+
+        // Calls the Hello method with a string argument
+        greetings.Hello("Houston");
+    }
+}
 }
